b3ae6951b625477f3ac0229666959b578bd5f8c3
JFranco2021 <96149890+JFranco2021@users.noreply.github.com>
2021-12-16 23:45:42 -0600

    Ejercicio array 3

---
+++ arraysexercise3.js
@@ -0,0 +1,16 @@
+
+function arraysExercise3(){
+    numbers = [1,2,3,4];
+
+    //numbers = [1,2,3,4].map(function(x) {return x*2;});
+
+    
+    var sum = numbers[0] + numbers[1] + numbers[2] + numbers[3];
+    var multi = numbers[0] * numbers[1] * numbers[2] * numbers[3];
+    console.log("The sum of the array is " + sum);
+    console.log("The multiplication of the array is " + multi);
+    
+    //console.log(numbers);
+    return
+    }
+    arraysExercise3();
