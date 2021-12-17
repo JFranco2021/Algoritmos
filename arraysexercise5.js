e94398f14235a70bdb2cd3ae1667daf0e1ef86df
JFranco2021 <96149890+JFranco2021@users.noreply.github.com>
2021-12-17 00:06:52 -0600

    Ejercicio array 5

--- arraysexercise5.js
+++ arraysexercise5.js
@@ -1,16 +1,21 @@
-
 function arraysExercise5(){
     var people = ["Maria","Dani","Luis","Juan","Camila"];
     console.log(people); // Write a command that prints out all of the people in the list.
 
-    var value = people.indexOf("Juan"); //Write the command to remove "Dani" from the array.
-    console.log(value);
-    people.splice(value,value+1);
+    var value = people.indexOf("Dani"); //Write the command to remove "Dani" from the array.
+
+    delete people[value];
     console.log(people);
 
-    value = people.indexOf("Dani"); //Write the command to remove "Juan" from the array.
-    people.splice(value,value);
+    value = people.indexOf("Juan"); //Write the command to remove "Juan" from the array.
+    delete people[value];
     console.log(people);
+
+    people.push("Salvador");
+    console.log(people);
+
+    var value = people.indexOf("Maria"); 
+    console.log("Maria se encuentra en la posicion: "+value);
 
     return;
     }
