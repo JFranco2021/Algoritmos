cf43b0a60af07c7cce8bedb9bcb7343cea066961
JFranco2021 <96149890+JFranco2021@users.noreply.github.com>
2021-12-16 23:41:41 -0600

    Ejercicio array 1

---
+++ arraysexercise1.js
@@ -0,0 +1,9 @@
+
+var arr = ["This", "is", "a", "sentence."];
+function printOutString() {
+    // your code here
+    console.log(arr[0] + " " + arr[1] + " " + arr[2] + " " + arr[3]);
+}
+
+printOutString();
+
