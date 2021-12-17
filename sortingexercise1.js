104221a2293e27b9f907c0ea595c59d1459c66f0
JFranco2021 <96149890+JFranco2021@users.noreply.github.com>
2021-12-17 00:12:20 -0600

    Ejercicio sorting 1

---
+++ sortingexercise1.js
@@ -0,0 +1,8 @@
+const colores = ['Rojo', 'Verde', 'Azul', 'Amarillo'];
+console.log("Colores sin ordenar");
+console.log(colores);
+
+valor = colores.sort();
+console.log("Colores ordenados alfabeticamente");
+console.log(valor);
+return;
