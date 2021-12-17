4d7ae96440e8ec26ea1e018cb58af3165d4eba55
JFranco2021 <96149890+JFranco2021@users.noreply.github.com>
2021-12-16 23:47:30 -0600

    Ejercicio array 5

---
+++ arraysexercise5.js
@@ -0,0 +1,17 @@
+
+function arraysExercise5(){
+    var people = ["Maria","Dani","Luis","Juan","Camila"];
+    console.log(people); // Write a command that prints out all of the people in the list.
+
+    var value = people.indexOf("Juan"); //Write the command to remove "Dani" from the array.
+    console.log(value);
+    people.splice(value,value+1);
+    console.log(people);
+
+    value = people.indexOf("Dani"); //Write the command to remove "Juan" from the array.
+    people.splice(value,value);
+    console.log(people);
+
+    return;
+    }
+    arraysExercise5();
