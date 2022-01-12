d305473d5a88b393608032cf8f1321d2eb3784e5
JFranco2021 <96149890+JFranco2021@users.noreply.github.com>
2022-01-11 20:52:17 -0600

    Variables y tipo de datos

---
+++ variablesYtipoDeDatos/js.js
@@ -0,0 +1,198 @@
+//Part 1
+var x = false;
+console.log(x);
+
+var y = Number(x);
+console.log(typeof(y));
+
+var z = String(x)
+console.log(typeof(z));
+
+console.log(typeof(x));
+
+
+var x = true;
+console.log(x);
+var y = Number(x)
+console.log(typeof(y));
+
+var z = String(x)
+console.log(typeof(z))
+
+console.log(typeof(x));
+
+
+var x = 0;
+console.log(x);
+console.log(typeof(x));
+
+var z = String(x)
+console.log(typeof(z));
+
+var x = !!"false";
+console.log(typeof(x));
+
+
+var x = 1;
+console.log(x);
+console.log(typeof(x));
+
+var z = String(x)
+console.log(typeof(z));
+
+var x = !!"false";
+console.log(typeof(x));
+
+
+var x = "0";
+console.log(x);
+
+var y = Number(x);
+console.log(typeof(y));
+
+var z = String(x)
+console.log(typeof(z));
+
+var x = !!"false";
+console.log(typeof(x));
+
+
+var x = "000";
+console.log(x);
+
+var y = Number(x);
+console.log(typeof(y));
+
+var z = String(x)
+
+console.log(typeof(z));
+
+var x = !!"false";
+console.log(typeof(x));
+
+
+var x = "1";
+console.log(x);
+
+var y = Number(x);
+console.log(typeof(y));
+
+var z = String(x)
+console.log(typeof(z));
+
+var x = !!"false";
+console.log(typeof(x));
+
+
+var x = NaN;
+console.log(x);
+
+var y = Number(x);
+console.log(typeof(y));
+
+
+
+var z = String(x)
+console.log(typeof(z));
+
+var x = !!"false";
+console.log(typeof(x));
+
+
+var x = Infinity;
+console.log(x);
+
+var y = Number(x);
+console.log(typeof(y));
+
+var z = String(x)
+console.log(typeof(z));
+
+var x = !!"false";
+console.log(typeof(x));
+
+
+var x = -Infinity;
+console.log(x);
+
+var y = Number(x);
+console.log(typeof(y));
+
+var z = String(x)
+console.log(typeof(z));
+
+var x = !!"false";
+console.log(typeof(x));
+
+
+var x = "";
+console.log(x);
+
+var y = Number(x);
+console.log(typeof(y));
+
+var z = String(x)
+console.log(typeof(z));
+
+var x = !!"false";
+console.log(typeof(x));
+
+
+var x = "20";
+console.log(x);
+
+var y = Number(x);
+console.log(typeof(y));
+
+var z = String(x)
+console.log(typeof(z));
+
+var x = !!"false";
+console.log(typeof(x));
+
+
+var x = null;
+console.log(x);
+
+var y = Number(x);
+console.log(typeof(y));
+
+var z = String(x)
+console.log(typeof(z));
+
+var x = !!"false";
+console.log(typeof(x));
+
+
+var x = undefined;
+console.log(x);
+
+var y = Number(x);
+console.log(typeof(y));
+
+var z = String(x)
+console.log(typeof(z));
+
+var x = !!"false";
+console.log(typeof(x));
+
+//Part 2 Calculator
+
+
+
+var añoNacimiento = 1993;
+var añoFuturo  = 2050;
+var edad  = añoFuturo - añoNacimiento;
+console.log('Yo tendre ' + edad + ' o ' + (edad - 1) + ' años');
+
+/*card*/
+
+var name = "Salvador";
+var locati = "GDL";
+var hobby = "eating";
+
+console.log("Hi, my name is "+ name + ". I live in " +locati+ " and enjoy "+ hobby);
+
+
+
+
