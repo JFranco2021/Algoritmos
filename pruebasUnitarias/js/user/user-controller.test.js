00d6fb5096e147090b73edbe0d79b2a00c706454
JFranco2021 <96149890+JFranco2021@users.noreply.github.com>
2022-01-26 22:32:55 -0600

    pruebas unitarias parte 3

--- pruebasUnitarias/js/user/user-controller.test.js
+++ pruebasUnitarias/js/user/user-controller.test.js
@@ -16,9 +16,35 @@ test('remove user to userController', () => {
     expect(userController.users).not.toContain(user);
   });
 
-  /*
-test('Usuario ya existe',() => {
-  expect(userController.findById(1234)).toBe(1234);
-})
+  //Implement 1 test for the add function that verifies a user that is not in the users' list.
 
-*/
+test('add user to userController', () => {    
+  let user = new User(4321,"Salvador", "salvador@generation.org");
+  userController.add(user);    
+  expect(userController.getUsers()).toContain(user);
+});
+
+//Implement 1 test for the remove function that verifies a user that is not in the users' list.
+
+test('remove user to userController', () => {    
+  let user = new User(4321,"Salvador", "salvador@generation.org");
+  userController.add(user);    
+  userController.remove(user);
+  expect(userController.users).not.toContain(user);
+});
+
+//Implement 2 unit tests for the findByEmail function.
+
+test('test for the findByEmail function', () => {    
+  let user = new User(4321,"Salvador", "salvador@generation.org");
+  userController.findByEmail(user);    
+  expect(userController.users).toContainEqual(user);
+});
+
+//Implement 2 unit tests for the findById function.
+
+test('tests for the findById function', () => {    
+  let user = new User(4321,"Salvador", "salvador@generation.org");
+  userController.findById(user);    
+  expect(userController.users).toContainEqual(user);
+});
